2aa60c911bb32d16887ab7a8a091d9ecc1c022c5
ishola360 <isholah360@gmail.com>
2023-02-06 18:07:43 -0800

    image display

--- src/component/header/images.jsx
+++ src/component/header/images.jsx
@@ -1,21 +1,21 @@
 export const Images = [
     {
       id: 1,
-      url: "/assets/bar.jpeg",
+      url: "assets/bar.jpeg",
       title: "INTERIOR DESIGN",
       design: "INNOVATIVE",
       agency:" Getting the right design before gettting the design right, our design a simple, but significant"
     },
     {
       id: 2,
-      url: "/assets/bed.jpeg",
+      url: "assets/bed.jpeg",
       title: "UNIQUE DESIGN",
       design: "ELEGANT",
       agency:" Getting the right design before gettting the design right, our design a simple, but significant"
     },
     {
         id: 3,
-        url: "/assets/din.jpeg",
+        url: "assets/din.jpeg",
         title: "DESIGN AGENCY",
         design: "INTERIOR",
         agency:" Getting the right design before gettting the design right, our design a simple, but significant"
@@ -23,7 +23,7 @@ export const Images = [
       },
     {
         id:4,
-        url: "/assets/lvr.jpeg",
+        url: "assets/lvr.jpeg",
         title: "MORDERN DESIGN", 
         design: "SCINTILLATING", 
         agency:" Getting the right design before gettting the design right, our design a simple, but significant"
